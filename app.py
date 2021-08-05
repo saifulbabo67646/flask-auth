be8666835d696d22fe597a3fdc65b01cf8334410
saifulbabo67646 <abbo67646@gmail.com>
2021-08-05 21:22:24 +0600

    User class added with get user by id and delete user by id feature

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.scaffold import F
 from flask_restful import Api
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -16,13 +16,16 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
